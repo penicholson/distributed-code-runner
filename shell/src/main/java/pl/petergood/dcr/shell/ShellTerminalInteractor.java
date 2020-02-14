485a98419f6d1be1b678229c0b08c9a4222a324b
Peter Nicholson <petergoods@hotmail.com>
2020-02-14 20:33:43 +0100

    Restructure modules

--- shell/src/main/java/pl/petergood/dcr/shell/ShellTerminalInteractor.java
+++ shell/src/main/java/pl/petergood/dcr/shell/ShellTerminalInteractor.java
@@ -8,7 +8,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class ShellTerminalInteractor implements TerminalInteractor {
     @Override
